cb24456de0ef97352e0463496afd6d019b838739
Vladislav Pestsov <pestsov.js@gmail.com>
2022-11-18 09:56:38 +0200

    fix(Docs): fix _sidebar

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,25 +1,25 @@
-interface IEnv {
+interface IEnvReader {
     /**
      * @description Set specify config to `process.env`
-     * @param configPath - absolute path to config file
+     * @param path - absolute path to config file
      */
-    setConfig(configPath?: string): void;
+    setConfig(path?: string): void;
     /**
      * @description Set all private variables who have into configs to `process.env`.
-     * @param configPath - absolute path to folder with configs. if configPath is missed - envReader has been read the "config" folder who can be created into app root folder.
+     * @param path - absolute path to folder with configs. if configPath is missed - envReader has been read the "config" folder who can be created into app root folder.
      */
-    setConfigs(configPath?: string): void;
+    setConfigs(path?: string): void;
     /**
      * @description Get specify private variable into `process.env`
      * @typeParam T - Data type. Can be set the number string or boolean if variableName should be this type.
-     * @param variableName - Key private variable
+     * @param name - Key private variable
      * @returns Private variable with specify data type
      * */
-    get<T extends number | string | boolean>(variableName: string): T;
+    get<T extends number | string | boolean>(name: string): T;
 }
 
 export type PrivateVariables = {
     [key: string]: string;
 };
 
-export default IEnv
+export default IEnvReader
